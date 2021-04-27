1e1a02934ee2911c2b5cf52f1092d7b28ec3f02d
Mateo Verdaguer <mateoverdaguer123@gmail.com>
2021-04-27 00:50:08 -0300

    added user login, changes in contact info and ccs updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+let listaProductos = ``;
+// guarda la lista de clientes del local storage en una variable
+let listaClientes = JSON.parse(localStorage.getItem('clientes'));
+
+const ingresarNombreCliente = () => {
+  let nombreCliente = prompt("Ingrese su nombre: ");
+  let numeroCliente = Math.floor(Math.random() * 1000);
+  // crea un objeto nuevo con los datos del cliente y lo agrega al array de clientes
+  listaClientes.push(new Cliente(numeroCliente, nombreCliente));
+  // guarda la lista actualizada en el local storage
+  localStorage.setItem('clientes', JSON.stringify(listaClientes));
+
+  console.log(JSON.parse(localStorage.getItem('clientes')));
+};
+
+
+// animacion del burger menu (mobile)
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
@@ -7,7 +24,7 @@ const navSlide = () => {
     // toggle nav
     nav.classList.toggle("nav-active");
 
-    // animate links
+    // animacion de links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
@@ -17,10 +34,53 @@ const navSlide = () => {
         }s`;
       }
     });
-    // burger animation
     burger.classList.toggle('toggle');
   });
-  ``;
 };
 
-navSlide();
+// accede a la data del JSON y muestra los productos
+$(function() {
+  $.getJSON('data.json', function(data) {
+    console.log(data.productos);
+    console.log(data.productos.length);
+    $(() => {
+      for(let i = 0; i < data.productos.length; i++) {
+        console.log('vuelta ' + i);
+        listaProductos += `
+          <div class="product">
+            <img
+              class="product-img"
+              src="${data.productos[i].img}"
+              alt="imagen-del-producto"/>
+              <div class="product-info">
+                <h3 class="product-title">${data.productos[i].nombre}</h3>
+                <h4 class="product-price">$${data.productos[i].precio}</h4>
+              </div>
+          </div>`;
+        console.log(data.productos[i]);
+        // $('ul').append('<li>' + data.productos[i].nombre + ' ' + data.productos[i].precio + '</li>');
+        };
+    });
+    document.getElementById("productos").innerHTML = listaProductos;
+  }).error(function() {
+    console.log('error');
+  });
+});
+
+function onInit() {
+  navSlide();
+
+  ingresarNombreCliente();
+  console.log(listaProductos);
+
+  console.log('lista de productos ' + listaProductos);
+
+}
+
+/** Crea funcion constructora de objetos */
+function Cliente(numeroCliente, nombre) {
+  this.numeroCliente = numeroCliente;
+  this.nombre = nombre;
+}
+
+onInit();
